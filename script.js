f1a918081f69f51eff2cc441602f8ffcae28c693
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-17 14:59:43 +0000

    update

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
 
+    // navBar changing color scrolling function
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass("bg-white")
+            $('.navbar-nav .nav-link').css('color', 'black');
+        } else {
+            $('.navbar').removeClass("bg-white")
+            $('.navbar-nav .nav-link').css('color', 'white');
+        }
+    })
+
+    // date changing function
+    var currentYear = new Date().getFullYear();
+    $('#date').html(currentYear);
+
+
 });
